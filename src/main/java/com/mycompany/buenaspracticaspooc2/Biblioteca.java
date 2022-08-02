191ddf0520f9a2ea5fb73fd6c8f7a13e38829e0f
Aiker Acosta <aikiringo26@gmail.com>
2022-08-01 20:23:11 -0500

    Initializing the Biblioteca class

--- src/main/java/com/mycompany/buenaspracticaspooc2/Biblioteca.java
+++ src/main/java/com/mycompany/buenaspracticaspooc2/Biblioteca.java
@@ -1,10 +1,28 @@
 
 package com.mycompany.buenaspracticaspooc2;
 
+//Import arraylist
+import java.util.ArrayList;
+
 /**
  *
  * @author maicol
  */
 public class Biblioteca {
-    
+    //Create a private arraylist of Cancion objects
+    private ArrayList<Cancion> canciones = new ArrayList<Cancion>();
+
+    //Getter and setter
+    public ArrayList<Cancion> getCanciones() {
+        return canciones;
+    }
+
+    public void setCanciones(ArrayList<Cancion> canciones) {
+        this.canciones = canciones;
+    }
+
+    //Public constructor with no parameters
+    public Biblioteca() {
+        //
+    }
 }
