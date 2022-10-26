52ec49c978f9ef57c5a5d6248c0f436d084b5e11
ArangoMike <michaelarango0531@gmail.com>
2022-10-26 15:51:14 -0500

    Desarrollo de la clase PlayList. Se desarrolla toda la clase y sus metodos para crear el playlist personalizado, e imprimir en pantalla dicho playlist.

--- src/main/java/com/mycompany/buenaspracticaspooc2/PlayList.java
+++ src/main/java/com/mycompany/buenaspracticaspooc2/PlayList.java
@@ -1,15 +1,63 @@
 package com.mycompany.buenaspracticaspooc2;
 
+// Imports
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class PlayList extends Biblioteca {
 
-    //Public constructor with no parameters
+    // Creación de ArrayList de canciones para la playlist presonalizada.
+    protected ArrayList<Cancion> playList = new ArrayList<>();
+    // variable donde se alojara las opciones elejidas por el usuario
+    int op;
+    // variable usada para llenar el playlist personalizado.
+    int desicion = 1;
+    // Creación del objeto Scanner
+    Scanner in = new Scanner(System.in);
+
+    //Constructor publico sin parametros
     public PlayList() {
         //
     }
 
-    //Create a method to add a Cancion Object to the PlayList
-    public void addCancion(Cancion cancion) {
-        this.getCanciones().add(cancion);
+    // Creación de metodo para crear el playlist personalizado.
+    public void nuevaPlayList() {
+        if (desicion == 1) {
+            while (desicion == 1) {
+                System.out.println("¿Que canción quieres agregar al nuevo Playlist?");
+                System.out.println("Indicame su ID");
+                op = in.nextInt();
+
+                for (int i = 0; i < canciones.size(); i++) {
+
+                    if (op == canciones.get(i).getId()) {
+                        playList.add(canciones.get(i));
+                    }
+                }
+
+                System.out.println("registro exitoso!");
+                System.out.println("¿seguir agregando canciones al nuevo Playlist?");
+                System.out.println("Responda 1 para si o 2 para no.");
+                desicion = in.nextInt();
+            }
+        } else {
+            System.out.println("Playlist ya creada, eliminala para crear una nueva.");
+        }
     }
 
+    // Metodo para Imprimir todos los objetos contenidos en el arrayList.
+    public void imprimirPlayList() {
+
+        // Ciclo for para recorrer el ArrayList
+        for (int i = 0; i < playList.size(); i++) {
+
+            // Imprimimos en patantalla el contenido de los objetos usando el toString.
+            System.out.println((i + 1) + ". ");
+            System.out.println(playList.get(i).toString());
+        }
+    }
+
+    public ArrayList<Cancion> getPlayList() {
+        return playList;
+    }
 }
