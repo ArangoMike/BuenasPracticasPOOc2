d55bfd8c6cf69a04db2659b6da304a050711813c
ArangoMike <michaelarango0531@gmail.com>
2022-10-28 15:39:25 -0500

    Implementacion del metodo compareTo En la clase canción para comparar la duración de cada canción.

--- src/main/java/com/mycompany/buenaspracticaspooc2/Cancion.java
+++ src/main/java/com/mycompany/buenaspracticaspooc2/Cancion.java
@@ -1,100 +1,101 @@
-
 package com.mycompany.buenaspracticaspooc2;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
 
+public class Cancion implements Comparable<Cancion> {
 
-public class Cancion {
-        //titulo, identificador, fecha, duración, genero, caratula y una descripción corta.
-        private String title;
-        private int id;
-        private LocalDate date;
-        private LocalTime duration;
-        private Genres genre;
-        private String cover;
-        private String info;
-    
-        public Cancion(String title, int id, LocalDate date, LocalTime duration, Genres genre, String cover, String info) {
-            this.title = title;
-            this.id = id;
-            this.date = date;
-            this.duration = duration;
-            this.genre = genre;
-            this.cover = cover;
-            this.info = info;
-        }
-    
-        //Create a public constructor with only id
-        public Cancion(int id) {
-            this.id = id;
-        }
-    
-        //Getters and setters for all the attributes
-        public String getTitle() {
-            return title;
-        }
-    
-        public void setTitle(String title) {
-            this.title = title;
-        }
-    
-        public int getId() {
-            return id;
-        }
-    
-        public void setId(int id) {
-            this.id = id;
-        }
-    
-        public LocalDate getDate() {
-            return date;
-        }
-    
-        public void setDate(LocalDate date) {
-            this.date = date;
-        }
-    
-        public LocalTime getDuration() {
-            return duration;
-        }
-    
-        public void setDuration(LocalTime duration) {
-            this.duration = duration;
-        }
-    
-        public Genres getGenre() {
-            return genre;
-        }
-    
-        public void setGenre(Genres genre) {
-            this.genre = genre;
-        }
-    
-        public String getCover() {
-            return cover;
-        }
-    
-        public void setCover(String cover) {
-            this.cover = cover;
-        }
-    
-        public String getInfo() {
-            return info;
-        }
-    
-        public void setInfo(String info) {
-            this.info = info;
-        }
+    //Título, identificador, fecha, duración, genero, caratula y una descripción corta.
+    private String title;
+    private int id;
+    private LocalDate date;
+    private LocalTime duration;
+    private Genres genre;
+    private String cover;
+    private String info;
 
-    /**
-     *
-     * metodo ToString de la clase Cancion.
-     */
+    // Constructor de la clase cancion con parametros.
+    public Cancion(String title, int id, LocalDate date, LocalTime duration, Genres genre, String cover, String info) {
+        this.title = title;
+        this.id = id;
+        this.date = date;
+        this.duration = duration;
+        this.genre = genre;
+        this.cover = cover;
+        this.info = info;
+    }
+
+    //Constructor de la clase canción solo con parametro id.
+    public Cancion(int id) {
+        this.id = id;
+    }
+
+    //Getters y setters para todos los atributos.
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public LocalTime getDuration() {
+        return duration;
+    }
+
+    public void setDuration(LocalTime duration) {
+        this.duration = duration;
+    }
+
+    public Genres getGenre() {
+        return genre;
+    }
+
+    public void setGenre(Genres genre) {
+        this.genre = genre;
+    }
+
+    public String getCover() {
+        return cover;
+    }
+
+    public void setCover(String cover) {
+        this.cover = cover;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
+
+    // Método compareTo para comparar el atributo duración de la clase canción.
+    @Override
+    public int compareTo(Cancion o) {
+        return o.getDuration().compareTo(duration);
+    }
+
+    // Método ToString de la clase Cancion. 
     @Override
     public String toString() {
-        return "Título: " + title + "\nID: " + id + "\nFecha: " + date + "\nDuración: " + duration + "\nGénero: " + genre + "\nCarátula: " + cover + "\nDescripción: " + info ;
+        return "Título: " + title + "\nID: " + id + "\nFecha: " + date + "\nDuración: " + duration + "\nGénero: " + genre + "\nCarátula: " + cover + "\nDescripción: " + info;
     }
-    
-    
 }
--- src/main/java/com/mycompany/buenaspracticaspooc2/CompararFecha.java
+++ src/main/java/com/mycompany/buenaspracticaspooc2/CompararFecha.java
@@ -1,15 +1,12 @@
 
 package com.mycompany.buenaspracticaspooc2;
 
-import com.mycompany.buenaspracticaspooc2.Cancion;
+
 import java.util.Comparator;
 
-/**
- *
- * @author maicol
- */
 public class CompararFecha implements Comparator<Cancion>{
 
+    // Método compare  usado para comparar el atributo date de la clase Cancion.
     @Override
     public int compare(Cancion o1, Cancion o2) {
         return o1.getDate().compareTo(o2.getDate());   
